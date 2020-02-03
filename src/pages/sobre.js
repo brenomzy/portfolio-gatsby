ce95eaff2edd5a0c7816e22a965311a28a6dd2d1
Breno Daroz <brenomzy@gmail.com>
2020-02-03 18:39:56 -0300

    Sobre
    Criada e estilizada a página 'Sobre'

--- src/pages/sobre.js
+++ src/pages/sobre.js
@@ -7,8 +7,86 @@ class Sobre extends React.Component {
   render() {
     return (
       <Layout location={this.props.location}>
-        <SEO title="Portfolio" />
-        <div>Teste</div>
+        <SEO title="Sobre" />
+        <div className="bg-gray-100">
+          <div className="container mx-auto py-16">
+            <h2 className="text-7xl font-bold text-gray-900 mb-1 relative">
+              Olá, meu nome é Breno<span className="text-orange-400">.</span>
+            </h2>
+            <p className="mt-12 text-gray-900 text-xl leading-relaxed max-w-3xl">
+              Sou um UX/UI & Frontend Designer de Maringá-PR, focado em criar
+              experiências e solucionar problemas com design. Faço parte da
+              internet criando websites desde a era em que se usavam{" "}
+              <img
+                className="inline-block h-10"
+                src={"/botao-web20.png"}
+                alt="Botão antigo"
+              />
+              .
+            </p>
+
+            <p className="mt-6 text-gray-900 text-xl leading-relaxed max-w-3xl">
+              Desenvolvo wireframes, protótipos e experiências prontas para uso
+              que melhoram a facilidade de aprendizado com visuais modernos e
+              acessíveis. No meu tempo livre, gosto de aprender novas
+              tecnologias como{" "}
+              <a
+                href="https://www.gatsbyjs.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-semibold text-purple-600"
+              >
+                GatsbyJS
+              </a>
+              , que utilizei para desenvolver este website.
+            </p>
+
+            <p className="mt-6 text-gray-900 text-xl leading-relaxed max-w-3xl">
+              Trabalho atualmente na{" "}
+              <a
+                className="font-semibold text-red-600"
+                href="http://www.sabium.com.br"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Sabium Sistemas
+              </a>
+              , onde faço parte da solução de diversos problemas interessantes
+              na área de varejo.
+            </p>
+          </div>
+        </div>
+
+        <div className="container mx-auto">
+          <h3 className="mt-16 font-semibold text-gray-900 text-lg leading-relaxed max-w-3xl">
+            Seguem abaixo alguns dos softwares e tecnologias que tenho utilizado
+            ultimamente:
+          </h3>
+          <ul className="mt-6 flex justify-between max-w-xs text-gray-900 font-medium">
+            <div>
+              <li>
+                <span className="text-orange-500">‣</span> Figma
+              </li>
+              <li>
+                <span className="text-orange-500">‣</span> Adobe XD
+              </li>
+              <li>
+                <span className="text-orange-500">‣</span> HTML/CSS
+              </li>
+            </div>
+            <div>
+              <li>
+                <span className="text-orange-500">‣</span> Javascript
+              </li>
+              <li>
+                <span className="text-orange-500">‣</span> React
+              </li>
+              <li>
+                <span className="text-orange-500">‣</span> VueJS
+              </li>
+            </div>
+          </ul>
+        </div>
       </Layout>
     )
   }
